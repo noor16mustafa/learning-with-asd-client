8c172ace108a0700f3ec2b4611fe7a4a376ec351
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-10-27 12:17:05 +0600

    footer added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer';
 
 import Navbar from '../Shared/Navbar';
 
@@ -8,6 +9,7 @@ const Main = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
 
         </div>
     );
